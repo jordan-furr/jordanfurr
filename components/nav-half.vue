647ed2c48c7c3aff9e380332d73fda720077b8cd
jordan-furr <jordanfurr98@gmail.com>
2024-11-23 16:28:50 +0100

    pages

--- components/nav-half.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="w-100 pv2 pb3 borderbox">
-        <div class="h-20">
-            <img class="w-100" src="/img/blue-jordanfurr.png" alt="">
-        </div>
-        <div class="flex flex-col space-between h-75">
-        </div>
-    </div>
-</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,28 @@
 <template>
-    <div class="">
-        
+    <div class="essays menu-page flex flew-row">
+        <div class="w-33 nav-half-cont">
+            <div class="w-100 pv2 pb3 borderbox">
+                <div class="h-20">
+                    <img class="w-100" src="/img/pink-jordanfurr.png" alt="">
+                </div>
+                <div class="flex flex-col space-between h-75">
+                </div>
+            </div>
+        </div>
+        <div class="w-66 ph6 borderbox cont-half-cont">
+            <div class="description">
+                <h2>about</h2>
+                <p>Jordan Ellis Furr is a xxxtra multi passionate creator.</p>
+            </div>
+        </div>
         <div class="about-btn">
+            <nuxt-link to="/about" class="word-link" exact-active-class="active">
+                <p>about</p>
+            </nuxt-link>
+        </div>
+        <div class="jordan-btn">
             <nuxt-link to="/" class="word-link" exact-active-class="active">
-                <p>home</p>
+                <h1>Jordan Furr</h1>
             </nuxt-link>
         </div>
     </div>
