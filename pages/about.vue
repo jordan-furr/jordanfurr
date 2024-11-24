918f990064b22c42c9b528dd7961689005400b14
jordan-furr <jordanfurr98@gmail.com>
2024-11-24 19:26:02 +0100

    height

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="essays menu-page flex flew-row">
-        <div class="w-33 nav-half-cont">
-            <div class="w-100 pv2 pb3 borderbox">
+    <div class="essays menu-page flex flew-row h-100">
+        <div class="w-33 nav-half-cont h-100">
+            <div class="w-100 pv2 pb3 borderbox h-100">
                 <div class="sidestar-cont">
                     <img class="sidestar-image" src="/img/pink-jordanfurr.png" alt="JF in a STAR">
                 </div>
@@ -38,6 +38,7 @@
             <div class="description">
                 <h2>about</h2>
                 <p>Jordan Ellis Furr is a xxxtra multi passionate creator.</p>
+                <p>jordan@jordanfurr.com</p>
             </div>
         </div>
         <div class="about-btn">
